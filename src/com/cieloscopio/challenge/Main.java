475803a978d157a33455196bc3e16e2fe02d0959
V1k770r <testprueba066@gmail.com>
2024-06-24 12:03:44 -0500

    Initial commit

---
+++ src/com/cieloscopio/challenge/Main.java
@@ -0,0 +1,28 @@
+package com.cieloscopio.challenge;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("Tome su desicion!");
+
+        Scanner sc = new Scanner(System.in);
+
+        Boolean value = sc.nextBoolean();
+
+        while(value){
+            System.out.println("empezo el programa");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
